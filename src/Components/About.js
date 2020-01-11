1614ddaff057d3e04513482d8779e126c7660660
quahjieren@gmail.com <quahjieren@gmail.com>
2020-01-11 13:39:54 +0800

    Update links to open on a new tab

--- src/Components/About.js
+++ src/Components/About.js
@@ -39,7 +39,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume in PDF</a>
+                     <a target="_blank" href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume in PDF</a>
                   </p>
                </div>
             </div>
